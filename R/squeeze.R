dea7bb805bc6466bdc437e7d7af854d3119a8d47
María Bueno Álvez <buenoalvezm@gmail.com>
2023-06-20 16:10:03 +0200

    squeeze() function

--- R/squeeze.R
+++ R/squeeze.R
@@ -1,15 +1,27 @@
-#' Retrieve proteomics and transcriptomics data for studies at cBioPortal
+#' Extract studies from cbioportal with the specifcied type of data and minimum number of samples
 #'
-#' @param study_id cBioPortal study identifier
+#' @param n Minimum number of samples
+#' @param ... Data types
 #'
-#' @return Proteomics and transcriptomics data from the selected study
+#' @return Tibble with all studies
 #' @export
 #'
 #' @examples
-squeeze <-
-  function(study_id) {
+#' squeeze(n = 20, massSpectrometrySampleCount, mrnaRnaSeqSampleCount)
+squeeze <- function(n, ...){
 
+  all_studies <-read_csv("data/all_studies_info.csv")
 
+  # Find studies with specified data
+  all_studies_info <-
+    all_studies |>
+    select(studyId, name, cancerTypeId, referenceGenome, importDate,...)
 
-    return(proteomics_data, transcriptomics_data)
-  }
+  # Filter according to minimum sample size
+  candidate_studies <-
+    all_studies_info |>
+    filter(if_all(6:last_col(), ~ . > n))
+
+  return(candidate_studies)
+
+}
