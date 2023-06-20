9ed1c32bee51d7e4607aa85e32c42f689c88d8e8
María Bueno Álvez <buenoalvezm@gmail.com>
2023-06-20 15:01:06 +0200

    squeeze_data() and squeeze_metadata() functions

---
+++ R/squeeze.R
@@ -0,0 +1,15 @@
+#' Retrieve proteomics and transcriptomics data for studies at cBioPortal
+#'
+#' @param study_id cBioPortal study identifier
+#'
+#' @return Proteomics and transcriptomics data from the selected study
+#' @export
+#'
+#' @examples
+squeeze <-
+  function(study_id) {
+
+
+
+    return(proteomics_data, transcriptomics_data)
+  }
