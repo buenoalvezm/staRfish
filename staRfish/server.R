d81bdb994242119e64fc6f712fef28702d78abd4
CarolinaPB <carolina.pitabarros@gmail.com>
2023-06-21 09:39:21 +0200

    added table filtering

--- staRfish/server.R
+++ staRfish/server.R
@@ -7,7 +7,7 @@ library(cbioportalR)
 library(magrittr)
 
 source("../R/grab.R")
-source("../R/process.R")
+source("../R/digest.R")
 source("../R/squeeze.R")
 source("../R/release_metadata.R")
 
@@ -18,22 +18,20 @@ function(input, output, session) {
     squeeze(n = input$n_samples, unlist(.))
     })
 
-  output$study_table <- renderTable({all_studies()})
+  output$study_table <- DT::renderDT({all_studies()})
   study_ids <- reactive({all_studies() %>% pull(studyId)})
 
   observe({
     updateSelectInput(session, "select_study", choices =   study_ids())
   })
 
-  metadata_table <- renderTable({release_metadata(input$select_study)})
+  metadata_table <- DT::renderDT({release_metadata(input$select_study)})
   output$metadata_table <- metadata_table
 
-  output$download_metadata <- {downloadHandler(
+  output$download_metadata <- downloadHandler(
     filename = "metadata.tsv",
     content = function(fname){
       write_tsv(release_metadata(input$select_study), fname)
     })
 
-
-  }
 }
