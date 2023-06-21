3eee13d435397ccd2f3b645e14168e582f4ef8e1
CarolinaPB <carolina.pitabarros@gmail.com>
2023-06-21 14:00:01 +0200

    added file

---
+++ R/squeeze_metadata.R
@@ -0,0 +1,14 @@
+#' Title
+#'
+#' @param study_id The cbioportal study ID
+#'
+#' @return Tibble with the study metadata
+#' @export
+#'
+#' @examples
+#' squeeze_metadata(study_id = "brca_cptac_2020")
+squeeze_metadata <- function(study_id) {
+  cbio <- cBioPortal()
+  metadata <- clinicalData(cbio, study_id)
+  return(metadata)
+}
