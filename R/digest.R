f1846461caacc7f824b8c7348f4150e3dca0300a
abmarstrand <adamboegh@gmail.com>
2023-06-21 10:17:17 +0000

    Added gene correlation with clinical variables, improved code structure

--- R/digest.R
+++ R/digest.R
@@ -23,6 +23,15 @@
 #rna <- read_tsv("/home/rstudio/staRfish/data_test/data_mrna_seq_fpkm.txt") %>% dplyr::rename("gene"=Hugo_Symbol)
 #metadata <- read_tsv("/home/rstudio/staRfish/data_test/brca_cptac_2020_clinical_data.tsv") %>% select(-c(`Study ID`,`Patient ID`))
 require(tidyverse)
+require(clusterProfiler)
+require(org.Hs.eg.db)
+require(reshape2)
+require(ggplot2)
+require(viridis)
+require(ggpubr)
+require(ggbeeswarm)
+require(ggrepel)
+
 ### DATA PROCESSING ###
 gather_rna_prot_data <- function(rna,protein) {
   gathered_data <- dplyr::inner_join(x=protein,y=rna,by="gene",suffix=c(".prot",".rna"))
@@ -49,9 +58,6 @@ create_rna_prot_correlation <- function(rna,protein) {
 }
 
 create_pw_df <- function(corr_df) {
-  require(clusterProfiler)
-  require(org.Hs.eg.db)
-
   genes <- corr_df$gene
 
   gene_ids <- mapIds(org.Hs.eg.db, keys=genes, column="ENTREZID", keytype="SYMBOL", multiVals="first")
@@ -62,7 +68,7 @@ create_pw_df <- function(corr_df) {
                                  minGSSize    = 1,
                                  maxGSSize    = 500)
 
-  kegg_df <- as.data.frame(kegg_annotations) %>% select(ID,Description,geneID)
+  kegg_df <- as.data.frame(kegg_annotations) %>% dplyr::select(ID,Description,geneID)
 
   expanded_kegg_df <- kegg_df %>%
     mutate(geneID = strsplit(as.character(geneID), "/")) %>%
@@ -70,10 +76,10 @@ create_pw_df <- function(corr_df) {
     group_by(ID) %>%
     mutate(gene_index = row_number()) %>%
     pivot_wider(names_from = gene_index, values_from = geneID, names_prefix = "gene_") %>%
-    select(-ID) %>%
-    pivot_longer(!Description) %>% ungroup() %>% select(-name) %>% na.omit() %>%
+    dplyr::select(-ID) %>%
+    pivot_longer(!Description) %>% ungroup() %>% dplyr::select(-name) %>% na.omit() %>%
     mutate(value=getSYMBOL(as.character(value),data='org.Hs.eg')) %>%
-    rename(gene=value)
+    dplyr::rename(gene=value)
 
   combined_pw_data <- corr_df %>%
     inner_join(expanded_kegg_df, by = "gene")
@@ -83,10 +89,8 @@ create_pw_df <- function(corr_df) {
 
 ### START PLOTTING DATA ###
 cor_matrix_samples <- function(gathered_data) {
-  require(reshape2)
-  require(ggplot2)
-  require(viridis)
-  gathered <- gathered_data %>% select(-gene) %>% na.omit()
+
+  gathered <- gathered_data %>% dplyr::select(-gene) %>% na.omit()
   cormat <- round(cor(gathered),2)
   mcormat <- melt(cormat)
 
@@ -114,6 +118,8 @@ correlation_plot_levels <- function(correlations_df) {
     geom_point(stat = "identity") +
     theme(axis.title.x= element_blank(),
           axis.text.x = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.text = element_text(),
           panel.background = element_rect(fill="white"),
           legend.position = "none",
           plot.title= element_text(hjust = 0.5)) +
@@ -154,7 +160,7 @@ KEGG_correlation_plot <- function(combined_pw_data) {
 }
 
 plot_gene <- function(gathered_data,gene_name) {
-  require(ggpubr)
+
   stopifnot("Gene not available in dataset" = gene_name %in% gathered_data$gene)
   gene_tib <- gathered_data %>% filter(gene==gene_name) %>%
     pivot_longer(!gene,names_to=c("patient","type"),names_sep="\\.",values_to="expr") %>%
@@ -167,28 +173,15 @@ plot_gene <- function(gathered_data,gene_name) {
     ggtitle(gene_name) +
     theme(panel.background = element_rect(fill="white"),
           legend.position = "none",
-          plot.title= element_text(hjust = 0.5)) +
+          plot.title= element_text(hjust = 0.5),
+          axis.line = element_line(),
+          axis.text = element_text()) +
     xlab("Protein expression") +
     ylab("RNA expression")
 
 }
 
 metadata_correlations <- function(metadata,gathered_data,clin_var,gene_name) {
-  gene_tib <- gathered_data %>% filter(gene==gene_name) %>%
-    pivot_longer(!gene,names_to=c("patient","type"),names_sep="\\.",values_to="expr") %>%
-    pivot_wider(id_cols="patient",names_from="type",values_from="expr") %>% na.omit()
-  meta_corr_df <- metadata %>% rename("Sample ID"="patient") %>%
-    left_join(.,gene_tib,by="patient",suffix=c("","")) %>%
-    dplyr::select(all_of(clin_var),prot,rna) %>% na.omit()
-  stopifnot("Clinical variable not found or does not have enough data" = {(!(colnames(meta_corr_df[1])=="rna" | colnames(meta_corr_df[1]=="prot")))
-    !(length(meta_corr_df[[1]])==0)})
-
-  max_first  <- max(meta_corr_df$rna)   # Specify max of first y axis
-  max_second <- max(meta_corr_df$prot) # Specify max of second y axis
-  min_first  <- min(meta_corr_df$rna)   # Specify min of first y axis
-  min_second <- min(meta_corr_df$prot) # Specify min of second y axis
-  scale = (max_second - min_second)/(max_first - min_first)
-  shift = min_first - min_second
   # Function to scale secondary axis
   scale_function <- function(x, scale, shift){
     return ((x)*scale - shift)
@@ -198,13 +191,84 @@ metadata_correlations <- function(metadata,gathered_data,clin_var,gene_name) {
   inv_scale_function <- function(x, scale, shift){
     return ((x + shift)/scale)
   }
+  gene_tib <- gathered_data %>% filter(gene==gene_name) %>%
+    pivot_longer(!gene,names_to=c("patient","type"),names_sep="\\.",values_to="expr") %>%
+    pivot_wider(id_cols="patient",names_from="type",values_from="expr") %>% na.omit()
+  meta_corr_df <- metadata %>% rename("Sample ID"="patient") %>%
+    left_join(.,gene_tib,by="patient",suffix=c("","")) %>%
+    dplyr::select(all_of(clin_var),prot,rna) %>% na.omit()
+  stopifnot("Clinical variable not found or does not have enough data" = (!(colnames(meta_corr_df[1])=="rna" | colnames(meta_corr_df[1])=="prot")))
 
-  if (meta_corr_df[[1]])
-  ggplot(meta_corr_df,aes(x=get(clin_var),y=rna)) +
-    geom_point(color="#7FBF7B") +
-    geom_point(aes(y=inv_scale_function(prot,scale,shift)),color="#AF8DC3") +
-    scale_y_continuous(limits = c(min_first, max_first), sec.axis = sec_axis(~scale_function(., scale, shift), name="Protein level")) +
-    labs(x = "Clinical variable", y = "RNA expression", color = "")
+
+  max_first  <- max(meta_corr_df$rna)   # Specify max of first y axis
+  max_second <- max(meta_corr_df$prot) # Specify max of second y axis
+  min_first  <- min(meta_corr_df$rna)   # Specify min of first y axis
+  min_second <- min(meta_corr_df$prot) # Specify min of second y axis
+  scale = (max_second - min_second)/(max_first - min_first)
+  shift = min_first - min_second
 
 
+  if (!(is.numeric(meta_corr_df[[1]]))) {
+    meta_corr_df[[1]] <- factor(tolower(meta_corr_df[[1]]))
+    if(length(unique(meta_corr_df[[1]]))==2) {
+      p1 <- ggplot(meta_corr_df, aes(rna, ..count.., fill = .data[[clin_var]])) +
+        geom_density(position = "fill") +
+        labs(fill = as.name(clin_var)) +  theme(panel.background = element_rect(fill="white"),
+                                                legend.position = "none",
+                                                   plot.title= element_text(hjust = 0.5)) +
+        ggtitle("RNA") +
+        colorspace::scale_fill_discrete_divergingx(palette="Tropic")
+      p2 <- ggplot(meta_corr_df, aes(prot, ..count.., fill = .data[[clin_var]])) +
+        geom_density(position = "fill") +
+        labs(fill = as.name(clin_var)) + theme(panel.background = element_rect(fill="white"),
+                                                  plot.title= element_text(hjust = 0.5)) +
+        ggtitle("PROTEIN") +
+        colorspace::scale_fill_discrete_divergingx(palette="Tropic")
+      p <- p1+p2
+    } else if (length(unique(meta_corr_df[[1]]))>2) {
+      bp_dat <- meta_corr_df %>% mutate(prot=inv_scale_function(prot,scale,shift)) %>% melt()
+      p <- ggplot(bp_dat, aes(x=get(clin_var),y=value,color= variable,fill=variable)) +
+        geom_boxplot(alpha=0.1,outlier.size=0) +
+        geom_beeswarm(dodge.width=1) +
+        scale_y_continuous(limits = c(min_first, max_first), sec.axis = sec_axis(~scale_function(., scale, shift), name="Protein level")) +
+        labs(color = "Datatype", y = "RNA expression") +  theme(panel.background = element_rect(fill="white"),
+                                                plot.title= element_text(hjust = 0.5)) +
+        xlab(clin_var) +
+        colorspace::scale_fill_discrete_divergingx(palette="Tropic",rev=F) +
+        colorspace::scale_color_discrete_divergingx(palette="Tropic",rev=F) +
+        guides(fill = "none", colour = "legend")
+    }
+  } else {
+    if (checkbox) {
+      p <- ggplot(meta_corr_df,aes(x=get(clin_var),y=rna)) +
+        geom_point(color="#009B9F") +
+        geom_smooth(color="#009B9F",method="lm") +
+        geom_point(aes(y=inv_scale_function(prot,scale,shift)),color="#C75DAA") +
+        geom_smooth(aes(x=get(clin_var),y=inv_scale_function(prot,scale,shift)),color="#C75DAA",method="lm") +
+        theme(panel.background = element_rect(fill="white"),
+              plot.title= element_text(hjust = 0.5),
+              axis.line = element_line(),
+              axis.text = element_text()) +
+        scale_y_continuous(limits = c(min_first, max_first), sec.axis = sec_axis(~scale_function(., scale, shift), name="Protein level")) +
+        labs(x = clin_var, y = "RNA expression", color = "Datatype")
+    } else {
+      p <- ggplot(meta_corr_df,aes(x=get(clin_var),y=rna)) +
+        geom_point(color="#009B9F") +
+        geom_point(aes(y=inv_scale_function(prot,scale,shift)),color="#C75DAA") +
+        theme(panel.background = element_rect(fill="white"),
+              plot.title= element_text(hjust = 0.5),
+              axis.line = element_line(),
+              axis.text = element_text()) +
+        scale_y_continuous(limits = c(min_first, max_first), sec.axis = sec_axis(~scale_function(., scale, shift), name="Protein level")) +
+        labs(x = clin_var, y = "RNA expression", color = "Datatype")
+    }
+  }
+  if (exists("p")) {
+    return(p)
+  }
+}
+
+get_all_metadata_correlations <- function(metadata,gathered_data,gene_name) {
+  p_list <- map(colnames(metadata), \(x) metadata_correlations(metadata,gathered_data,x,gene_name))
+  return(p_list)
 }
